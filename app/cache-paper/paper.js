854e9e356b089074d938bd3345a7d10686948c36
u4bi <u4bi@u4bi.com>
2017-01-25 11:40:22 +0900

    cache-paper render singleton init

--- app/cache-paper/paper.js
+++ app/cache-paper/paper.js
@@ -1,21 +1,35 @@
-var stat = false;
+var cache_paper = (function (){
+    var instance;
+    var stat = false;
+    function init(){
+        return{
+          stat: stat,
+          on: function(){
+            alert('on');
+          },
+          remove : function(){
+            alert('remove');
+          },
+          flag : function(){
+              alert('flag');
+          }
+        };
+    }
+    return {
+      getInstance: function(){
+        if(!instance){
+          instance = init();
+        }
+        return instance;
+      }
+    }
+})();
 
-function paper() {
-    if(stat)return;
-    flag();
-    remove();
-}
+var paper = cache_paper.getInstance();
 
-function remove(){
-  chrome.browsingData.remove({"since":0}, {
-    "cache": true,
-  }, flag());
-}
-
-function flag(){
-    stat = !stat;
-    chrome.browserAction.setIcon({path:"icon-cache_"+stat+".png"});
-}
-
-chrome.browserAction.onClicked.addListener(paper);
-paper();
+chrome.browserAction.onClicked.addListener(function(){
+    alert('stat : '+ paper.stat);
+    paper.on();
+    paper.remove();
+    paper.flag();
+});
