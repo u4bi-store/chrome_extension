de51ab65fa8c949878a3b6f05d06dd4272bc824a
u4bi <u4bi@u4bi.com>
2017-01-25 09:12:50 +0000

    data name fix

--- app/cache-paper/js/paper.js
+++ app/cache-paper/js/paper.js
@@ -6,18 +6,18 @@ document.addEventListener('DOMContentLoaded', function () {
   console.log('dom conn init');
 
   data =[
-    {name: "appcache"    ,key: "appcache",    value: true},
-    {name: "cache"       ,key: "cache",       value: true},
-    {name: "cookies"     ,key: "cookies",     value: true},
-    {name: "downlods"    ,key: "downloads",   value: true},
-    {name: "fileSystems" ,key: "fileSystems", value: true},
-    {name: "formData"    ,key: "formData",    value: true},
-    {name: "history"     ,key: "history",     value: true},
-    {name: "indexedDB"   ,key: "indexedDB",   value: true},
-    {name: "localStorage",key: "localStorage",value: true},
-    {name: "pluginData"  ,key: "pluginData",  value: true},
-    {name: "passwords"   ,key: "passwords",   value: true},
-    {name: "webSQL"      ,key: "webSQL",      value: true}
+    {name: "응용 프로그램 캐시" ,key: "appcache",    value: true},
+    {name: "캐시된 이미지,파일" ,key: "cache",       value: true},
+    {name: "쿠키"            ,key: "cookies",     value: true},
+    {name: "다운로드 기록"     ,key: "downloads",   value: true},
+    {name: "파일 시스템즈"     ,key: "fileSystems", value: true},
+    {name: "폼 데이터"        ,key: "formData",    value: true},
+    {name: "히스토리 기록"     ,key: "history",     value: true},
+    {name: "indexedDB"      ,key: "indexedDB",   value: true},
+    {name: "로컬스토리지"      ,key: "localStorage",value: true},
+    {name: "플러그인 데이터"   ,key: "pluginData",  value: true},
+    {name: "비밀번호"         ,key: "passwords",   value: true},
+    {name: "webSQL"         ,key: "webSQL",      value: true}
   ];
   
   var box = document.getElementById('box');
