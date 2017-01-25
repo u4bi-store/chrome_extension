d479ab8dbd04c04c166d274d777f13252fabe260
u4bi <u4bi@u4bi.com>
2017-01-25 21:15:47 +0900

    cache paper render

--- app/cache-paper/background/back-paper.js
+++ app/cache-paper/background/back-paper.js
@@ -1,5 +1,6 @@
 var cache_paper = (function (){
     var instance;
+    var data = localStorage;
 
     function init(){
         return{
@@ -11,7 +12,20 @@ var cache_paper = (function (){
           remove : function(){
               chrome.browsingData.remove(
                   {"since": 0},
-                  {"cache": true}
+                  {
+                    "appcache"    :JSON.parse(data['appcache']),
+                    "cache"       :JSON.parse(data['cache']),
+                    "cookies"     :JSON.parse(data['cookies']),
+                    "downloads"   :JSON.parse(data['downloads']),
+                    "fileSystems" :JSON.parse(data['fileSystems']),
+                    "formData"    :JSON.parse(data['formData']),
+                    "history"     :JSON.parse(data['history']),
+                    "indexedDB"   :JSON.parse(data['indexedDB']),
+                    "localStorage":JSON.parse(data['localStorage']),
+                    "pluginData"  :JSON.parse(data['pluginData']),
+                    "passwords"   :JSON.parse(data['passwords']),
+                    "webSQL"      :JSON.parse(data['webSQL'])
+                  }
               , instance.flag);
           },
           flag : function(){
--- app/cache-paper/js/paper.js
+++ app/cache-paper/js/paper.js
@@ -4,22 +4,29 @@ var btnArray = [];
 
 document.addEventListener('DOMContentLoaded', function () {
   console.log('dom conn init');
-
+  
   data =[
-    {name: "응용 프로그램 캐시" ,key: "appcache",    value: true},
-    {name: "캐시된 이미지,파일" ,key: "cache",       value: true},
-    {name: "쿠키"            ,key: "cookies",     value: true},
-    {name: "다운로드 기록"     ,key: "downloads",   value: true},
-    {name: "파일 시스템즈"     ,key: "fileSystems", value: true},
-    {name: "폼 데이터"        ,key: "formData",    value: true},
-    {name: "히스토리 기록"     ,key: "history",     value: true},
-    {name: "indexedDB"      ,key: "indexedDB",   value: true},
-    {name: "로컬스토리지"      ,key: "localStorage",value: true},
-    {name: "플러그인 데이터"   ,key: "pluginData",  value: true},
-    {name: "비밀번호"         ,key: "passwords",   value: true},
-    {name: "webSQL"         ,key: "webSQL",      value: true}
+    {name: "응용 프로그램 캐시" ,key: "appcache",    value: false},
+    {name: "캐시된 이미지,파일" ,key: "cache",       value: false},
+    {name: "쿠키"            ,key: "cookies",     value: false},
+    {name: "다운로드 기록"     ,key: "downloads",   value: false},
+    {name: "파일 시스템즈"     ,key: "fileSystems", value: false},
+    {name: "폼 데이터"        ,key: "formData",    value: false},
+    {name: "히스토리 기록"     ,key: "history",     value: false},
+    {name: "indexedDB"      ,key: "indexedDB",   value: false},
+    {name: "로컬스토리지"      ,key: "localStorage",value: false},
+    {name: "플러그인 데이터"   ,key: "pluginData",  value: false},
+    {name: "비밀번호"         ,key: "passwords",   value: false},
+    {name: "webSQL"         ,key: "webSQL",      value: false}
   ];
   
+  for(var i=0; i<data.length; i++){
+    var temp = localStorage[data[i].key];
+    if(temp != undefined && temp != false){
+      data[i].value = JSON.parse(temp);
+    }
+  }
+
   var box = document.getElementById('box');
   box.innerHTML='web data paper';
   
