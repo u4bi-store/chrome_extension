eb760c4fa3f6537addfc73132267d834efd32812
u4bi <u4bi@u4bi.com>
2017-01-25 13:08:19 +0000

    paper code style fix

--- app/cache-paper/js/paper.js
+++ app/cache-paper/js/paper.js
@@ -1,11 +1,6 @@
-var data = [];
-var dataArray = [];
-var btnArray = [];
-
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('dom conn init');
-  
-  data =[
+var ctrl;
+var dataArray = [], btnArray = [];
+var data =[
     {name: "응용 프로그램 캐시" ,key: "appcache",    value: false},
     {name: "캐시된 이미지,파일" ,key: "cache",       value: false},
     {name: "쿠키"            ,key: "cookies",     value: false},
@@ -19,25 +14,43 @@ document.addEventListener('DOMContentLoaded', function () {
     {name: "비밀번호"         ,key: "passwords",   value: false},
     {name: "webSQL"         ,key: "webSQL",      value: false}
   ];
+
+
+var Control = function(){};
+
+Control.prototype = {
+  fixCheck : function(bool){
+    for(var i=0; i<data.length; i++){
+      data[i].type = bool;
+      dataArray[i].checked = data[i].type;
+      this.save(data[i].key, data[i].type);
+    }
+  },
+  save : function(key, value){
+    localStorage.setItem(key,value);
+  }
+};
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  ctrl = new Control();
   
   for(var i=0; i<data.length; i++){
     var temp = localStorage[data[i].key];
-    if(temp != undefined && temp != false){
-      data[i].value = JSON.parse(temp);
-    }
+    if(temp != undefined && temp != false)data[i].value = JSON.parse(temp);
   }
 
   var box = document.getElementById('box');
-  box.innerHTML='web data paper';
+  box.innerHTML='인터넷 사용기록 삭제';
   
   var ul = document.createElement('ul');
   box.appendChild(ul);
 
   for(var i=0; i<data.length; i++){
-
+    
       var li = document.createElement('li');
       ul.appendChild(li);
-
+    
       var label = document.createElement('label');
       label.innerHTML=data[i].name;
       li.appendChild(label);
@@ -45,31 +58,23 @@ document.addEventListener('DOMContentLoaded', function () {
       dataArray[i] = document.createElement("input");
       dataArray[i].setAttribute("type", "checkbox");
       dataArray[i].setAttribute("value", data[i].key);
-      dataArray[i].onclick=function(e){
-        save(e.target.attributes[1].nodeValue, e.target.checked);
-      };
       dataArray[i].checked = data[i].value;
-
+      dataArray[i].onclick=function(e){
+        ctrl.save(e.target.attributes[1].nodeValue, e.target.checked);
+      };
+      
       label.appendChild(dataArray[i]);
-      var removed=document.getElementById('removed');
-      var checked=document.getElementById('checked');
-
-      removed.onclick=function(){ fixCheck(false)};
-      checked.onclick=function(){ fixCheck(true)};
   }
   
+  var removed=document.getElementById('removed');
+  var checked=document.getElementById('checked');
+  
+  removed.onclick=function(){
+    ctrl.fixCheck(false);
+  };
+  
+  checked.onclick=function(){
+    ctrl.fixCheck(true);
+  };
+  
 });
-
-function fixCheck(bool){
-  for(var i=0; i<data.length; i++){
-    data[i].type = bool;
-    dataArray[i].checked = data[i].type;
-    save(data[i].key, data[i].type);
-  }
-}
-
-function save(key, value){
-  console.log(key+' '+value);
-  localStorage.setItem(key,value);
-  console.log(key+' saved : '+localStorage[key]);
-}
