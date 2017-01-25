1cb32f06289853ad6384b9c29765d68a359d4532
u4bi <u4bi@u4bi.com>
2017-01-25 15:13:48 +0900

    data render for dom

--- app/cache-paper/js/paper.js
+++ app/cache-paper/js/paper.js
@@ -1,5 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
   console.log('dom conn init');
+
+  var data =[
+    {name: "appcache",    type: true},
+    {name: "cache",       type: true},
+    {name: "cookies",     type: true},
+    {name: "downloads",   type: true},
+    {name: "fileSystems", type: true},
+    {name: "formData",    type: true},
+    {name: "history",     type: true},
+    {name: "indexedDB",   type: true},
+    {name: "localStorage",type: true},
+    {name: "pluginData",  type: true},
+    {name: "passwords",   type: true},
+    {name: "webSQL",      type: true}
+  ];
+  
+  var dataArray = [];
+  
   var box = document.getElementById("box");
-  box.innerHTML='dom box test';
+  box.innerHTML='web-data-paper';
+  
+  for(var i=0; i<data.length; i++){
+
+      var label = document.createElement("label");
+      label.innerHTML='<br>'+data[i].name;
+      box.appendChild(label);
+
+      dataArray[i] = document.createElement("input");
+      dataArray[i].setAttribute("type", "checkbox");
+      dataArray[i].setAttribute("value", data[i].name);
+      dataArray[i].checked = data[i].type;
+
+      label.appendChild(dataArray[i]);
+
+  }
 });
